384518e676c51447cbc8d6392439e54472ded5d0
Derek Garnett <drkgrntt@gmail.com>
2019-04-23 22:15:58 -0500

    add non-functioning submit button

--- lib/src/widgets/grocery_item_input.dart
+++ lib/src/widgets/grocery_item_input.dart
@@ -8,18 +8,55 @@ class GroceryItemInput extends StatelessWidget {
 
     final groceriesBloc = GroceriesProvider.of(context);
 
+    return Row(
+      children: [
+        textField(groceriesBloc),
+        submitButton(groceriesBloc),
+      ],
+    );
+  }
+
+
+  Widget textField(GroceriesBloc groceriesBloc) {
+
     return StreamBuilder(
       stream: groceriesBloc.groceryInputText,
       builder: (context, snapshot) {
-        return TextField(
-          onChanged: groceriesBloc.changeGroceryInputText,
-          decoration: InputDecoration(
-            hintText: 'Bread',
-            labelText: 'Grocery Item',
-            contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+
+        return Flexible(
+          child: TextField(
+            onChanged: groceriesBloc.changeGroceryInputText,
+            decoration: InputDecoration(
+              hintText: 'Bread',
+              labelText: 'Grocery Item',
+              contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+            ),
           ),
         );
       },
     );
+  }
+
+
+  Widget submitButton(GroceriesBloc groceriesBloc) {
+
+    return StreamBuilder(
+      stream: groceriesBloc.groceryInputText,
+      builder: (context, snapshot) {
+
+        return RaisedButton(
+          child: Text('Add'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          onPressed: () {
+            if (snapshot.data != null && snapshot.data != '') {
+              print('submitted!');
+            }
+          },
+        );
+      }
+    );
+    
+
   }
 }
